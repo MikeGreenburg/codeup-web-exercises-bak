a0e372156aa59c5a5655ceb004dbbc9bd9be3634
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-02-05 10:03:30 -0600

    fixed the previous days exercises with cleaner code

--- js/external-lec.js
+++ js/external-lec.js
@@ -1,17 +1,17 @@
 "use strict"
 
-Not widely used anymore with modern frameworks
-Alert makes a popup show up first on a page
+// Not widely used anymore with modern frameworks
+// Alert makes a popup show up first on a page
 alert("You are a winner");
 
-Confirm allows a user to select an option before an action happens
+// Confirm allows a user to select an option before an action happens
 confirm("Would you like to exit");
-set to a variable to capture what they want to do
+// set to a variable to capture what they want to do
 var userConfirm = confirm("Would you like to exit");
     console.log(userConfirm);
    alert("The user selected: " + userConfirm);
 
-Prompt is used to require the user to input something //
+// Prompt is used to require the user to input something
 var userPrompt = prompt("What is your favorite animal");
    // alert(userPrompt);
 
--- js/external_js.js
+++ js/external_js.js
@@ -2,51 +2,70 @@
 
 //console.log("hello from external javascript");
 
+// 1. Use the alert function to show a message that says 'Welcome to my Website!'.
+
+
 //alert("Welcome to my Website!");
 
+// 2. Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
+
+    // For example, if the user enters "blue", your code should alert a message that says: "Great, blue is my favorite color too!"
+
 //var userPrompt = prompt("What is your favorite color?");
-//alert("Great, " + userPrompt + " is my favorite color too!");
+// var alertMessage = "Great, " + userPrompt + " is my favorite color too!";
+//alert(alertMessage);
 
-// var priceOfRental = prompt("What is the rental price per night?");
-// var lMRental = prompt("How many nights do you want to rent The Little Mermaid?");
-// var bBRental = prompt("How many nights do you want to rent Brother Bear?");
-// var hercRental = prompt("How many nights do you want to rent Hercules?");
-// alert("Thank you. Your total charge will be: $" + ((lMRental * priceOfRental) + (bBRental * priceOfRental) + (hercRental * priceOfRental)));
+// 3. Complete exercise 3 from the previous lesson, but write your code in the external.js file instead of in the console.
 
-// var amaRate = prompt("How much did you make per hour at Amazon?"); //380//
-// var amaHours = prompt("How many hours did you work at Amazon?"); //10//
-// var googRate = prompt("How much did you make per hour at Google?"); //400//
-// var googHours = prompt("How many hours did you work at Google?"); //6//
-// var fbRate = prompt("How much did you make per hour at Facebook?"); //350//
-// var fbHours = prompt("How many hours did you work at Facebook?"); //4//
+    // When the exercise asks you to use a number, instead use a prompt to ask the user for that number
+    // Use an alert to show the results of each problem.
+
+// 1. Write some JavaScript code, that is, variables and operators, to describe the following scenarios. Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
 //
-// alert ("You made: $" + ((amaRate * amaHours) + (fbRate * fbHours) + (googRate * googHours))); //
+//     You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
 
-// var classSize = confirm("Is there still seating?");
-// console.log(classSize);
+// Pseudo code logic (*** write out how you are going to solve the problem before starting to code ***)
+// 3.1. movie rental question
+// 1. ask user what the rental fee is -> prompt & variable
+// 2. ask user how many days for LM -> prompt & variable
+// 3. ask user how many days for BB -> prompt & variable
+// 4. ask user how many days for H -> prompt & variable
+// 5. calculate -> rental fee * total days
+// 6. alert the total cost
+
+// var dailyRentalRate = Number(prompt("What is the rental price per night?"));
+// var littleMermaid = Number(prompt("How many nights do you want to rent The Little Mermaid?");
+// var brotherBear = Number(prompt("How many nights do you want to rent Brother Bear?"));
+// var hercules = Number(prompt("How many nights do you want to rent Hercules?"));
+// var totalCost = (littleMermaid + brotherBear + hercules) * dailyRentalRate;
+// alert("You owe :$" + totalCost);
+
+// 2. Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+
+// var amaRate = Number(prompt("How much did you make per hour at Amazon?")); //380//
+// var amaHours = Number(prompt("How many hours did you work at Amazon?")); //10//
+// var googRate = Number(prompt("How much did you make per hour at Google?")); //400//
+// var googHours = Number(prompt("How many hours did you work at Google?")); //6//
+// var fbRate = Number(prompt("How much did you make per hour at Facebook?")); //350//
+// var fbHours = Number(prompt("How many hours did you work at Facebook?")); //4//
+// var totalPay = (amaRate * amaHours) + (fbRate * fbHours) + (googRate * googHours);
+// var totalPay;
+// var alertMessage = "Your total pay is $" + totalPay;
+// alert(alertMessage)
+
+// 3. A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+
+// var classIsNotFull = confirm("Class is not full?");
+// var noScheduleConflicts = confirm("Schedules do not conflict");
 //
-// var classAtEightAM = confirm("Does your schedule conflict with any other classes at this time?");
-// console.log(classAtEightAM);
+// var studentEnrollmentStatus = classIsNotFull && noScheduleConflicts;
+// alert("Student can enroll: " + studentEnrollmentStatus);
+
+// 4. A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
+
+// var numberOfItems = Number(prompt("How many items are you buying?"));
+// var offerIsValid = confirm("Is the offer valid?");
+// var isPremiumMember = confirm("Are you a premium member?");
 //
-// function studentCanSignUpForClass() {
-//     if ((classSize === true) && (classAtEightAM === false)) {
-//     alert("You have been enrolled in this class");
-// } else {
-//     alert("Sorry, the selected class is full.");
-// }
-// }
-// studentCanSignUpForClass();
-
-var premiumMember = confirm("Are you a Premium Member?");
-        var numberOfItems = confirm("How many Items do you have?");
-        var offerValid = true;
-// var canHaveDiscount = (numberOfItems > 2 || premiumMember) && offerValid; canHaveDiscount = true
-
-        function canHaveDiscount() {
-            if ((premiumMember === true && numberOfItems <= 1) || offerValid === true); {
-                alert("Your discount has been applied.");
-            } else {
-                alert("Sorry your do not qualify for a discount");
-    }
-}
-canHaveDiscount();
+// var discountAppliedStatus = offerIsValid && (isPremiumMember === true || numberOfItems > 2);
+// alert("Can use discount: " + discountAppliedStatus);
