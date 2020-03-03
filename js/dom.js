e15d44a77d6fe3a3e395e1156a9fce1b4fe0bbe6
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-03-03 12:31:12 -0600

    worked more on DOM manipulation

--- js/dom.js
+++ js/dom.js
@@ -3,7 +3,7 @@
 // Javascript can be used to read/write/manipulate to the DOM
 // Object Oriented Representation
 
-//Examine the document object
+//******************************EXAMINE THE DOCUMENT OBJECT*****************************//
 //console.dir(document); // It shows us all of the different properties and methods attached to the document object
 //console.log(document.);// console.log(document. + whatever element you want to pass
 //document.all;// Gives us an HTML collection of everything that is inside the page/document
@@ -18,6 +18,67 @@
 //console.log(document.all[10].textContent = 'Hello');// will select the <h1> at index [10] and change it to "Hello" (you do not want to use this method of selecting from the dom as it is not dynamic!!!
 //console.log(document.forms);// grabs all the forms on the page. Use the index [0] to grab select items on th form
 //console.log(document.links);// grabs all the links on the page
+//console.log(document.images);// grabs all the images in the document
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!SELECTORS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+//*****************************GET ELEMENT BY ID************************************//
+
+//console.log(document.getElementById('headerTitle'));// pulls the header title and console logs it
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+//console.log(headerTitle);// puts header title inside a variable and the console.log just logs it.
+
+//headerTitle.textContent = 'Hello';// will change the header title text to 'Hello'
+//headerTitle.innerText = 'Goodbye';// will over ride the top one if used together. innerText pays attention to the styling
+//headerTitle.innerHTML = '<h3>Hello</h3>';// puts the <h3> inside the the <h1> and does not change the <h1>. It just puts the new element inside the old element (makes it a child of the element.).
+
+//******************************GET ELEMENTS BY Class NAME***************************//
+
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);// It gives us an HTMLCollection and each one has an index [0], [1], [2], etc..
+// console.log(items[1]);// used to access individual items (item 2)
+// items[1].textContent = 'Hello 2';// changes the text in the actual item index (2nd one)
+// items[1].style.fontWeight = 'bold';// changes the style of the text on that index item
+// items[1].style.backgroundColor = 'yellow';// changes the background color to 'yellow'
+//
+//###########################CHANGE ALL ELEMENTS BACKGROUND COLOR############################//
+//
+// GIVES ERROR
+// items.style.backgroundColor = 'lightgrey'; //does not work
+//
+// //CORRECT WAY
+// for(var i = 0; i < items.length; i++){
+//     items[i].style.backgroundColor = "#f4f4f4";
+// }
+
+
+//***********************************GET ELEMENTS BY TAGNAME***********************************//
+// works the same way as by ID and Class except you are getting it by TAG!
+
+//var li = document.getElementsByTagName('li');
+//console.log(items);// grabs all the li items by the index on the page and displays them to the console.
+//console.log(li[1]);// used to access individual items (item 2)
+//li[1].textContent = 'Hello 2';// changes the text in the actual item index (2nd one)
+//li[1].style.fontWeight = 'bold';// changes the style of the text on that index item
+//li[1].style.backgroundColor = 'yellow';// changes the background color to 'yellow'
+
+//############################CHANGE ALL ELEMENTS BACKGROUND COLOR##############################//
+
+//GIVES ERROR
+//li.style.backgroundColor = 'lightgrey'; //does not work
+
+//CORRECT WAY
+// for(var i = 0; i < li.length; i++){
+//     li[i].style.backgroundColor = "#f4f4f4";
+// }
+
+//######################################CSS STYLE CHANGES########################################//
+
+//headerTitle.style // is how you change .css styles
+//headerTitle.style.borderBottom = 'solid 3 px #000';// changes the header title style to have a black solid bottom when used in conjunction with var headerTitle = document.getElementById('header-title');
+//header.style.borderBotom = 'solid 3px #000';// this changes the actual header border when used in conjunction with  var header = document.getElementById('main-header');
 
 
 
