e4dd0802baf51707e5479d3649e69da02dc42340
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-03-03 09:06:45 -0600

    did some DOM practice

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+// DOCUMENT OBJECT MODEL
+// Tree of nodes/elements created by the browser
+// Javascript can be used to read/write/manipulate to the DOM
+// Object Oriented Representation
+
+//Examine the document object
+//console.dir(document); // It shows us all of the different properties and methods attached to the document object
+//console.log(document.);// console.log(document. + whatever element you want to pass
+//document.all;// Gives us an HTML collection of everything that is inside the page/document
+//console.log(document.domain);// will result in localhost (the loopback address)
+//console.log(document.URL);// returns the full url address (http://localhost:633)
+//console.log(document.title);// returns the title (Item Lister)
+//document.title = "123";// document.title = will change the title to whatever value we pass
+//console.log(document.doctype);// uses the html5 doctype
+//console.log(document.head);// grabs the head element and outputs it to the console and see everything that is in the head.
+//console.log(document.body);// grabs the body and outputs it to the console.
+//console.log(document.all[10]);// will grab the <h1> at index [10]
+//console.log(document.all[10].textContent = 'Hello');// will select the <h1> at index [10] and change it to "Hello" (you do not want to use this method of selecting from the dom as it is not dynamic!!!
+//console.log(document.forms);// grabs all the forms on the page. Use the index [0] to grab select items on th form
+//console.log(document.links);// grabs all the links on the page
+
+
+
